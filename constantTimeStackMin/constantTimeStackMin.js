06749f6a30d4d183ad8af09f2a6e6813def02318
Justin Greer <justin.t.greer1@gmail.com>
2022-01-14 19:56:30 -0800

    toy4

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -1,40 +1,83 @@
-/**
- * Write a stack using your preferred instantiation pattern. Implement a min function
- * that returns the minimum value of all the elements in the stack in constant time.stack.  
+/*
+Prompt: Write a stack using your preferred instantiation pattern
+Add: Implement a min function that returns the minimum value of all the elements in the stack
+i: there is no input for a constructor function
+o:  an object with methods including push/insert, pop/remove, size/length and minValue
+c: all functions and methods must have constant time complexity
+e: a stack is first in first out (FIFE)
 
- * All of the functions in the Stack should run in constant time!
- *
- * var example = new Stack()
- *  example.push(4)
- *  example.push(3)
- *  example.min() === 3
- *  example.push(3)
- *  example.push(2)
- *  example.push(2)
- *  example.min() === 2
- */
+Stack Class
+*/
 
-/**
-  * Stack Class
-  */
-  var Stack = function() {
+stackMethods = {};
+var Stack = function() {
+  var result =  Object.create(stackMethods);
+  result.top = 0;
+  result.min = 10000000000000000;
+  result.prevMin = 999999999999999;
+  return result;
+};
 
-  // add an item to the top of the stack
-    this.push = function(value) {
-    };
+var result = {};
 
-  // remove an item from the top of the stack
-    this.pop = function() {
-    };
+// add an item to the top of the stack and inc the top by 1
+result.push = function(value) {
+  if (value < this.min) {
+    this.preMin = this.min;
+    this.min = value;
+  }
+  this[this.top] = value;
+  this.top++;
+};
 
-  // return the number of items in the stack
-    this.size = function() {
-    };
-  
-  // return the minimum value in the stack
-    this.min = function() {
+// remove an item from the top of the stack using delete and dec the top by 1 - does pop have a return value
+result.pop = function() {
+  //if the value at the top of the stack is the smallest value then set the min value equal to the prev value
+  if (this[this.top] === this.min) {
+    this.min = this.prevMin;
+  }
+  delete this[this.top];
+  //top minus 1
+  this.top--;
+  //return the now current value at the top of the stack
+  return this[this.top];
+};
 
-    };
+// return the number of items in the stack
+result.size = function() {
+  if (this.top <= 0) {
+    return 0;
+  }
+  return this.top;
+};
 
-  };
+// return the minimum value in the stack
+result.min = function() {
+  if (this.min !== undefined) {
+    return this.min;
+  }
+  return this.prevMin;
+};
 
+//TEST SUITE
+var test1 = new Stack()
+test1.push(4)
+test1.push(3)
+test1.min() === 3
+console.log('test 1 using push/min should return 3: ', test1.min());
+console.log('test 1 size method should be 2', test1.size());
+
+var test2 = new Stack();
+test2.push(3);
+test2.push(2);
+test2.push(2);
+console.log('test 2 using push/min should return 2: ', test2.min());
+
+var test3 = new Stack();
+test3.push(10);
+test3.push(5);
+test3.pop(5);
+test3.push(6);
+test3.push(8);
+console.log('test 3 using pop should return 6: ', test3.min());
+console.log('test 3 size method should be 3', test3.size());
--- hashTableResizing/hashTableResizing.js
+++ hashTableResizing/hashTableResizing.js
@@ -7,9 +7,10 @@
  * Resize by half whenever utilization drops below 1/4.
  */
 
-// This is a "hashing function". You don't need to worry about it, just use it
+// Below is a "hashing function". You don't need to worry about it, just use it
 // to turn any string into an integer that is well-distributed between
 // 0 and max - 1
+//hashTable already built
 var getIndexBelowMaxForKey = function(str, max) {
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
@@ -20,26 +21,122 @@ var getIndexBelowMaxForKey = function(str, max) {
   return hash % max;
 };
 
+/*
+prompt: write out the insert, remove, retrieve, and stoarge resizer functions as methods for the result object being created in the constructor function below
+i: no input
+o: output a properly built hashtable
+c: hashTable function is already built, but do need to build sotorage resizer function
+e: not worrrying about non-string inputs, and not using the keyword 'this' for the instantiation method used below
+explaination: build a hashtable that works properly
+visualization: refer to miro
+approximation: refer to pseudocode
+impementation: refer to code
+*/
+
 var makeHashTable = function() {
   var result = {};
   var storage = [];
   var storageLimit = 4;
   var size = 0;
   
-  result.insert = function(/*...*/ 
-) {
+  result.insert = function(key, value) {
     // TODO: implement `insert`
+    //if you add then add one to the size
+    size++;
+    var index = getIndexBelowMaxForKey(key, storageLimit)
+    var bucket = storage[index];
+    //if the bucket contains content
+    if (bucket) {
+      //iterate through each tupel array in the bucket
+      for (var tupel of bucket) {
+        //if the input key matches the 0 indexed item in the bucket
+        if (tupel[0] === tupel) {
+          //return the pair value property at index 1
+          tupel[1] = value;
+        }
+      } //finish iterating through entire bucket
+      //if the key wasn't found then the key value pair is not in the bucket so push to insert it
+      bucket.push([key, value]);
+    //otherwise the bucket is empty or nothing
+    } else {
+      //the storage[index] is an empty array
+      //push the given key value pair in an array into the bucket
+      storage[index].push([key, value]);
+    }
+    //double the storage limit as soon as the total number of items stored is greater than 3/4th of the number of slots in the storage array
+    if (size / storageLimit >= 0.75) {
+      storageResizer(storageLimit * 2);
+    }
+
   };
 
-  result.retrieve = function(/*...*/ 
-) {
+  result.retrieve = function(key) {
     // TODO: implement `retrieve`
+    var index = getIndexBelowMaxForKey(key, storageLimit)
+    var bucket = storage[index];
+    if (bucket) {
+      for (var tupel of bucket) {
+        //if the input key matches the 0 indexed item in the bucket
+        if (tupel[0] === tupel) {
+          //return the pair value property at index 1
+          return tupel[1];
+        }
+      }
+    }
+    return undefined;
   };
 
-  result.remove = function(/*...*/ 
-) {
-    // TODO: implement `remove`
+  result.remove = function(key) {
+    var index = getIndexBelowMaxForKey(key, storageLimit)
+    var bucket = storage[index];
+    if (bucket) {
+      for (var tupel = 0; tupel < bucket.length; tupel++) {
+        //if the input key matches the 0 indexed item in the bucket
+        if (tupel[i][0] === key) {
+          //remove the tupel from the bucket
+          size--;
+          bucket.splice(i, 1);
+        }
+      }
+    }
+    // Resize by halfing the storage as the total number of items stored is less than 1/4th of the number of slots in the storage array
+    if (size / storageLimit <= 0.25) {
+      storageResizer(storageLimit / 2);
+    }
   };
 
+  //storageResizer() is given a new storage limit (an integer)
+  result.storageResizer = function(newSize) {
+    //save the old storage limit
+    var prevStorage = storageLimit;
+    //set the storageLimit equal to the param new size and incase its a decimal round up the value
+    storageLimit = Math.ceil(newSize);
+    //to create an entirely new storage array clear the storage
+    storage = [];
+    //iterate through the old storage array
+    for (var l = 0; l < prevStorage.length; l++) {
+      var index = getIndexBelowMaxForKey(bucket[l][0], storageLimit)
+      var newBucket = storage[index];
+      //if the new bucket has contents i.e. tupels
+      if (newBucket) {
+        //push thr given new key value pair in
+        newBucket.push([bucket[l][0], bucket[l][1]]);
+      } else {
+        //set the new bucket to an empty arr
+        newBucket = [];
+        //push the new key value pair in
+        newBucket.push([bucket[l][0], bucket[l][1]]);
+      }
+    }
+  }
   return result;
 };
+
+
+//TEST SUITE
+var hashTable = makeHashTable();
+console.log(hashTable);
+console.log(hashTable.insert);
+console.log(hashTable.remove);
+console.log(hashTable.retrieve);
+console.log(hashTable.storageResizer);
