3e61126f7585c1c9a0154f2a39de82185bdff164
Justin Greer <justin.t.greer1@gmail.com>
2022-03-17 19:30:16 -0700

    done with palindrome toy

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -9,17 +9,26 @@
 const longestPalindrome = function(sentence) {
   // split sentence into words
   sentence = sentence.split(' ');
+  //if there is only one word in sentence then return empty string
+  if (sentence.length < 2) {
+    if (isPalindrome(sentence[0])) {
+      return sentence[0];
+    }
+    return '';
+  }
   // iterate words and collect the palindromes
   let paliArr = [];
   for (let i = 0; i < sentence.length; i++) {
-  if (isPalindrome(sentence[i]) === true) {
-    paliArr.push(sentence[i]);
-  }
+    if (isPalindrome(sentence[i]) === true) {
+      paliArr.push(sentence[i]);
+    }
   // sort the list of palindromes by word length
   }
   paliArr = paliArr.sort(sortAscendingByLength);
   // return the largest item in the sorted list
-  return paliArr[paliArr.length -1];
+  let result = paliArr[paliArr.length -1];
+  result = (" " + result.trim() + " ");
+  return result;
 }
 
 function reverseString(string) {
@@ -49,12 +58,18 @@ function sortAscendingByLength(a, b) {
 // TESTS SUITE
 let mySentence1 = "racecar are bad hannah"
 let test1 = longestPalindrome(mySentence1);
-console.log('Should be racecar: ', test1);
+console.log('Should be racecar: ', typeof test1, test1);
 
 let mySentence2 = "kayak rotator";
 let test2 = longestPalindrome(mySentence2);
-console.log('Should be rotator: ', test2);
+console.log('Should be rotator: ', typeof test2, test2);
 
 let mySentence3 = "nan noon is redder";
 let test3 = longestPalindrome(mySentence3);
-console.log('should be redder: ', test3);
+console.log('should be redder: ', typeof test3, test3);
+
+let test4 = longestPalindrome('abc');
+console.log('type should be a string: ', typeof test4);
+
+let test5 = longestPalindrome('dad');
+console.log('type should be string: ', typeof test5, 'should be dad: ', test5);
